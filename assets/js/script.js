306a65237a504a8969135edaafc90b8adfeb03e3
Jordan <jordan.zagerman@drexel.edu>
2018-06-14 00:09:06 -0400

    Pretty Much done
    will look at it again tomorrow, before I turn it in, but it's pretty much where it needs to be.

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,16 +28,44 @@ audio.onpause = function() {
   playing = false;
 };
 
-
-
-
-
 var body = document.getElementById('body');
 
+// click on path to change effect
 
+var triggerLargePath = document.querySelector(".the-big-piece-desktop");
+var triggerLargePathMobile = document.querySelector(".the-big-piece-mobile");
+var triggerLargePathTablet = document.querySelector(".the-big-piece-tablet");
 
+console.log(triggerLargePath);
 
-DomReady.ready(function() {
-  document.getElementById('text').classList.remove('active');
+triggerLargePath.addEventListener('click', triggerAnimation);
+triggerLargePathMobile.addEventListener('click', triggerAnimation);
+triggerLargePathTablet.addEventListener('click', triggerAnimation);
 
-});
+function triggerAnimation(){
+  console.log("triggerAnimation worked" );
+
+      // Check to see if svgs have a class of .animate
+      if (triggerLargePath.classList.contains('animate-flash')) {
+    //  Remove .animate from svgs
+    triggerLargePath.classList.remove('animate-flash');
+    triggerLargePath.classList.add('animate-rainbow');
+    triggerLargePathMobile.classList.remove('animate-flash');
+    triggerLargePathMobile.classList.add('animate-rainbow');
+    triggerLargePathTablet.classList.remove('animate-flash');
+    triggerLargePathTablet.classList.add('animate-rainbow');
+    } else {
+    //     // Add class of .animate to both svgs
+    triggerLargePath.classList.add('animate-flash');
+    triggerLargePath.classList.remove('animate-rainbow');
+    triggerLargePathMobile.classList.add('animate-flash');
+    triggerLargePathMobile.classList.remove('animate-rainbow');
+    triggerLargePathTablet.classList.add('animate-flash');
+    triggerLargePathTablet.classList.remove('animate-rainbow');
+    }
+
+    // OR 
+    // toggleClass(microphoneIcon, 'animate');
+    // toggleClass(carIcon, 'animate');
+    // toggleText(triggerAnimationButton);
+}
